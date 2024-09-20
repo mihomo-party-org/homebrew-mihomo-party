15f0d5cf6d66930b7d371689935e7c2db0bc746b
pompurin404 <pompurin404@mihomo.party>
2024-09-20 07:37:09 +0000

    mihomo-party 1.2.22

--- Casks/mihomo-party.rb
+++ Casks/mihomo-party.rb
@@ -1,9 +1,9 @@
 cask "mihomo-party" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.2.21"
-  sha256 arm:   "d8847748779f02fd863b7a24b5c2e31131d8fc4b07d6424b7c59f7605701c03a",
-         intel: "81d744560e011555425096fccd0774d5453fc5de51256e96e5bff6b00e116de4"
+  version "1.2.22"
+  sha256 arm:   "86057f611da9377c4af571dd18ccf9b734188b281922aab886864f65507c331a",
+         intel: "7137b2ff76af6c833cf688752d94d39eab46d105fd8530915a816b3daf2c1c33"
 
   url "https://github.com/mihomo-party-org/mihomo-party/releases/download/v#{version}/mihomo-party-macos-#{version}-#{arch}.dmg"
   name "mihomo-party"
