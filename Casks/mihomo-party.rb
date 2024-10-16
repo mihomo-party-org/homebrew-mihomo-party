035ddfe38fe08c0abc9567365dca07c8e80d89ca
pompurin404 <pompurin404@mihomo.party>
2024-10-16 10:25:33 +0000

    mihomo-party 1.5.0

--- Casks/mihomo-party.rb
+++ Casks/mihomo-party.rb
@@ -1,17 +1,18 @@
 cask "mihomo-party" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.9"
-  sha256 arm:   "84f1e5609a77cce095a7b7b93ba6e29c1fcf969249de705d15529dc2bd710691",
-         intel: "2eda873fcfeecbf1a417a021b68f3b1b5ad51d31ddc62dcad9c74a013c18a3b8"
+  version "1.5.0"
+  sha256 arm:   "e68bd3a6659c9473b13f15fe006b8738068954f14407e3b186641f2631dfc76d",
+         intel: "8786453548e561d720f98738f2a42879d6e706f30fffdf6bcb51e2073cac7352"
 
   url "https://github.com/mihomo-party-org/mihomo-party/releases/download/v#{version}/mihomo-party-macos-#{version}-#{arch}.pkg"
-  pkg "mihomo-party-macos-#{version}-#{arch}.pkg"
-  uninstall pkgutil: "party.mihomo.app"
   name "Mihomo Party"
   desc "Another Mihomo GUI"
   homepage "https://github.com/mihomo-party-org/mihomo-party"
-  zap trash:  [
-        "~/Library/Application\ Support/mihomo-party",
-      ]
+
+  pkg "mihomo-party-macos-#{version}-#{arch}.pkg"
+
+  uninstall pkgutil: "party.mihomo.app"
+
+  zap trash: "~/Library/Application Support/mihomo-party"
 end
