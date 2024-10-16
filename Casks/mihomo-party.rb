f96563cd8764a3f156588bfadb4130fb39855f92
布丁狗~ <pompurin404@mihomo.party>
2024-10-16 18:18:55 +0800

    Update mihomo-party.rb

--- Casks/mihomo-party.rb
+++ Casks/mihomo-party.rb
@@ -6,10 +6,8 @@ cask "mihomo-party" do
          intel: "2eda873fcfeecbf1a417a021b68f3b1b5ad51d31ddc62dcad9c74a013c18a3b8"
 
   url "https://github.com/mihomo-party-org/mihomo-party/releases/download/v#{version}/mihomo-party-macos-#{version}-#{arch}.pkg"
+  pkg "mihomo-party-macos-#{version}-#{arch}.pkg"
   name "Mihomo Party"
   desc "Another Mihomo GUI"
   homepage "https://github.com/mihomo-party-org/mihomo-party"
-
-  app "Mihomo Party.app"
-  binary "#{appdir}/Mihomo Party.app/Contents/MacOS/Mihomo Party"
 end
