f2fd0886311bd644d0737bdffefc3e4d470194f8
布丁狗~ <pompurin404@mihomo.party>
2024-10-19 14:51:27 +0800

    Update mihomo-party.rb

--- Casks/mihomo-party.rb
+++ Casks/mihomo-party.rb
@@ -1,9 +1,8 @@
 cask "mihomo-party" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.5.2"
-
   on_catalina do
+    version "1.5.1"
     sha256 arm:   "35751acabb9806626549e87133002e6be67993731e6e5126697573bad952f8b8",
            intel: "53cb1e79d2a00eb767b798c6506bf767e8b5398abbd932c5b229aec2f821666d"
 
@@ -12,6 +11,7 @@ cask "mihomo-party" do
     pkg "mihomo-party-catalina-#{version}-#{arch}.pkg"
   end
   on_big_sur :or_newer do
+    version "1.5.1"
     sha256 arm:   "c82611cbe2d2108c353606de03afd8f9f123e0bee3c762f34ad1c10c15594a9e",
            intel: "3a0095945f9d0e4838ea2aaf70a129c5e163637d039442b1171f75fc585cac8c"
 
