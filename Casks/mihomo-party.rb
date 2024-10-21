4563cb9dde79cbb51f1b155421d3d59882437498
pompurin404 <pompurin404@mihomo.party>
2024-10-21 15:30:52 +0000

    mihomo-party 1.5.3

--- Casks/mihomo-party.rb
+++ Casks/mihomo-party.rb
@@ -2,7 +2,7 @@ cask "mihomo-party" do
   arch arm: "arm64", intel: "x64"
 
   on_catalina do
-    version "1.5.2"
+    version "1.5.3"
     sha256 arm:   "da50050205d3a7fde50da7afb7658d6a0806523e3c035338b930d8907568f901",
            intel: "546b67a023c7cd1f858e6fc2d730985ed2083774767696b99d0be9068e03174c"
 
@@ -11,9 +11,9 @@ cask "mihomo-party" do
     pkg "mihomo-party-catalina-#{version}-#{arch}.pkg"
   end
   on_big_sur :or_newer do
-    version "1.5.2"
-    sha256 arm:   "c82611cbe2d2108c353606de03afd8f9f123e0bee3c762f34ad1c10c15594a9e",
-           intel: "3a0095945f9d0e4838ea2aaf70a129c5e163637d039442b1171f75fc585cac8c"
+    version "1.5.3"
+    sha256 arm:   "5565b74b41e46f32601b0c769554e70641e33da25b4ba727b58a87007386348b",
+           intel: "b0dc683c2cce11b38613d766390da4e6c7a7d3e05e43d7d5e013817fb568bc47"
 
     url "https://github.com/mihomo-party-org/mihomo-party/releases/download/v#{version}/mihomo-party-macos-#{version}-#{arch}.pkg"
 
